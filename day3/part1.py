c05a5b5a022569096d0487007111973630bc16f2
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-03 15:04:51 +0100

    improved sol day3 part 1

--- day3/part1.py
+++ day3/part1.py
@@ -2,9 +2,8 @@ import re
 
 
 with open("./day3/input.txt", "r", encoding="utf-8") as file:
-    sum = 0
     matches = re.findall(r"mul[(](\d+),(\d+)[)]", file.read())
-    print("Matches:", matches[0][0])
-    
+    sum = 0
     for e in matches:
         sum += int(e[0]) * int(e[1])
+    print(sum)
