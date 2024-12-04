41f7fd706864c6d9b9634126bd5e755928b10ae0
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-05 00:46:40 +0100

    done day 4 part 1

--- day4/part1.py
+++ day4/part1.py
@@ -1,24 +1,25 @@
 import re
 
+def findXMAS(string):
+    return re.findall(r"(?=(XMAS))|(?=(SAMX))", string)
+
 with open("./day4/input.txt", "r") as f:
     arr = f.readlines()
     cols = len(arr[0]) - 1
     sum = 0
     
     for row in arr:   #horizontal
-        if (len(re.findall(r"XMAS|SAMX", row))):
-            sum += 1
-
+        sum += len(findXMAS(row))
+    
     for col in range(0, cols):   #vertical
         ver = ""
         for row in arr:
             ver += row[col]
 
-        if (len(re.findall(r"XMAS|SAMX", ver))):
-            sum += 1
-            
-    diagNum = 0  # diagonal r to l, top half
-    while diagNum < cols:
+        sum += len(findXMAS(ver))   
+
+    diagNum = 1  # diagonal r to l, top half
+    while diagNum < cols-2:
         r = 0
         diag = ""
         while r < len(arr)-diagNum:
@@ -26,9 +27,8 @@ with open("./day4/input.txt", "r") as f:
             r += 1
         
         diagNum += 1
-        
-        if (len(re.findall(r"XMAS|SAMX", diag))):
-            sum += 1
+        sum += len(findXMAS(diag))
+
     
     diagNum = 0  # diagonal r to l, bottom half
     while diagNum < cols:
@@ -40,42 +40,39 @@ with open("./day4/input.txt", "r") as f:
 
         diagNum += 1
         
-        if (len(re.findall(r"XMAS|SAMX", diag))):
-            sum += 1
+        sum += len(findXMAS(diag))
+
     
     diagNum = 0
     tmpCols = cols
-    while diagNum < cols+5: # diagonal l to r, top half
+    while diagNum < cols-2: # diagonal l to r, top half
         r = 0
         diag = ""
 
         while r < len(arr)-diagNum:
-            diag += arr[r][cols-r-1]
+            diag += arr[r][tmpCols-r-1]
             r += 1
 
-        diagNum +=1
-        cols -= 1
-        print(diag)
-        if (len(re.findall(r"XMAS|SAMX", diag))):
-            sum += 1
+        diagNum += 1
+        tmpCols -= 1
+        sum += len(findXMAS(diag))
+
     
-    diagNum = 0
-    tmpCols = cols
-    while diagNum < cols: # diagonal l to r, top half
-        r = 0
+    diagNum = 1
+    while diagNum < cols-2: # diagonal l to r, bottom half
+        r = diagNum
+        j = 0
         diag = ""
 
-        while r < len(arr)-diagNum:
-            diag += arr[r+diagNum][cols-1-diagNum]
+        while r < len(arr):
+            diag += arr[r][cols-1-j]
             r += 1
-        
-        diagNum +=1
-        cols -= 1
-        if (len(re.findall(r"XMAS|SAMX", diag))):
-            sum += 1
-    
-    
+            j += 1
 
+        diagNum += 1
+        sum += len(findXMAS(diag))
+
+    
                     
     print(sum)
 
