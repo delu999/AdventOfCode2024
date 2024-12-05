1f28e0528a3a3a829c335c696a36f32d517cc335
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-05 02:38:36 +0100

    improved sol day4 part2

--- day4/part2.py
+++ day4/part2.py
@@ -5,13 +5,15 @@ for i in range(len(a)-2):
     for j in range(len(a[0])-2):
         if a[i+1][j+1] != 'A':
             continue
-        if a[i][j] == 'M' and a[i][j+2] == 'M' and a[i+2][j] == 'S' and a[i+2][j+2] == 'S':
-            sum+=1
-        elif a[i][j] == 'S' and a[i][j+2] == 'S' and a[i+2][j] == 'M' and a[i+2][j+2] == 'M':
-            sum+=1
-        elif a[i][j] == 'M' and a[i][j+2] == 'S' and a[i+2][j] == 'M' and a[i+2][j+2] == 'S':
-            sum+=1
-        elif a[i][j] == 'S' and a[i][j+2] == 'M' and a[i+2][j] == 'S' and a[i+2][j+2] == 'M':
-            sum+=1
+        if a[i][j] == 'S':
+            if a[i][j+2] == 'S' and a[i+2][j] == 'M' and a[i+2][j+2] == 'M':
+                sum+=1
+            elif a[i][j+2] == 'M' and a[i+2][j] == 'S' and a[i+2][j+2] == 'M':
+                sum+=1
+        if a[i][j] == 'M':
+            if a[i][j+2] == 'M' and a[i+2][j] == 'S' and a[i+2][j+2] == 'S':
+                sum+=1
+            elif a[i][j+2] == 'S' and a[i+2][j] == 'M' and a[i+2][j+2] == 'S':
+                sum+=1
 
 print(sum)
