536a68a70595df2d2d6163b131e04713b2ffb16d
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-08 01:33:09 +0100

    day7 improved

--- day7/part1.py
+++ day7/part1.py
@@ -1,21 +1,29 @@
+import time
+start = time.time()
 with open("./day7/input.txt", "r") as f:
     file = f.readlines()
 
-tot = 0
+maxOp = 0
 for i, line in enumerate(file):
+    blank_spaces = len(line.split(" ")) - 2
+    if (blank_spaces > maxOp):
+        maxOp = blank_spaces
+print(maxOp)
+a = 1
+maxPerm = 2 ** maxOp
+permutations = [[0 for _ in range(maxOp)] for _ in range(maxPerm)]
+
+for i in range(maxOp-1, -1, -1):
+    b = a
+    a *= 2
+    for j in range(maxPerm):
+        permutations[j][i] = "+" if j % a < b else "*"
+
+tot = 0
+for _, line in enumerate(file):
     operands = line.split(" ")
-    result = int(operands[0][:-1])
     blank_spaces = len(operands) - 2
     totPerm = 2 ** blank_spaces
-    permutations = [[0 for _ in range(blank_spaces)] for _ in range(totPerm)]
-    
-    a = 1
-    for i in range(blank_spaces-1, -1, -1):
-        b = a
-        a *= 2
-        for j in range(totPerm):
-            permutations[j][i] = "+" if j % a < b else "*"
-
     for i in range(totPerm):
         tmpTot = int(operands[1])
         for j in range(blank_spaces):
@@ -25,7 +33,9 @@ for i, line in enumerate(file):
                 tmpTot = tmpTot if tmpTot != 0 else 1
                 tmpTot = tmpTot * int(operands[j+2])
     
-        if result == tmpTot:
-            tot += result
+        if int(operands[0][:-1]) == tmpTot:
+            tot += tmpTot
             break
 print(tot)
+end = time.time()
+print(end - start)
