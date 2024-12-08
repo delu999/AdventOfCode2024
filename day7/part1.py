c216fb4f2750b7aa21aa8f89842183bde6e6ab9e
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-08 01:56:59 +0100

    day7 improved

--- day7/part1.py
+++ day7/part1.py
@@ -1,5 +1,3 @@
-import time
-start = time.time()
 with open("./day7/input.txt", "r") as f:
     file = f.readlines()
 
@@ -8,15 +6,15 @@ for i, line in enumerate(file):
     blank_spaces = len(line.split(" ")) - 2
     if (blank_spaces > maxOp):
         maxOp = blank_spaces
-print(maxOp)
+
 a = 1
-maxPerm = 2 ** maxOp
-permutations = [[0 for _ in range(maxOp)] for _ in range(maxPerm)]
+totPerm = 2 ** maxOp
+permutations = [[0 for _ in range(maxOp)] for _ in range(totPerm)]
 
 for i in range(maxOp-1, -1, -1):
     b = a
     a *= 2
-    for j in range(maxPerm):
+    for j in range(totPerm):
         permutations[j][i] = "+" if j % a < b else "*"
 
 tot = 0
@@ -24,18 +22,19 @@ for _, line in enumerate(file):
     operands = line.split(" ")
     blank_spaces = len(operands) - 2
     totPerm = 2 ** blank_spaces
+    
     for i in range(totPerm):
         tmpTot = int(operands[1])
-        for j in range(blank_spaces):
+        k = 1
+        for j in range(maxOp - blank_spaces, maxOp):
+            k += 1
             if permutations[i][j] == "+":
-                tmpTot = tmpTot + int(operands[j+2])
+                tmpTot = tmpTot + int(operands[k])
             else:
                 tmpTot = tmpTot if tmpTot != 0 else 1
-                tmpTot = tmpTot * int(operands[j+2])
+                tmpTot = tmpTot * int(operands[k])
     
         if int(operands[0][:-1]) == tmpTot:
             tot += tmpTot
             break
 print(tot)
-end = time.time()
-print(end - start)
--- day7/part2.py
+++ day7/part2.py
@@ -1,37 +1,47 @@
 with open("./day7/input.txt", "r") as f:
     file = f.readlines()
 
-tot = 0
+maxOp = 0
 for i, line in enumerate(file):
+    blank_spaces = len(line.split(" ")) - 2
+    if (blank_spaces > maxOp):
+        maxOp = blank_spaces
+
+a = 1
+totPerm = 3 ** maxOp
+permutations = [[0 for _ in range(maxOp)] for _ in range(totPerm)]
+
+for i in range(maxOp-1, -1, -1):
+    b = a
+    a *= 3
+    for j in range(totPerm):
+        if j % a < b:
+            permutations[j][i] = "+"
+        elif j % a >= a-b:
+            permutations[j][i] = "|"
+        else:
+            permutations[j][i] = "*"
+
+tot = 0
+for _, line in enumerate(file):
     operands = line.split(" ")
-    result = int(operands[0][:-1])
     blank_spaces = len(operands) - 2
     totPerm = 3 ** blank_spaces
-    permutations = [[0 for _ in range(blank_spaces)] for _ in range(totPerm)]
     
-    a = 1
-    for i in range(blank_spaces-1, -1, -1):
-        b = a
-        a *= 3
-        for j in range(totPerm):
-            if j % a < b:
-                permutations[j][i] = "+"
-            elif j % a >= a-b:
-                permutations[j][i] = "|"
-            else:
-                permutations[j][i] = "*"
-
     for i in range(totPerm):
         tmpTot = int(operands[1])
-        for j in range(blank_spaces):
+        k = 1
+        for j in range(maxOp - blank_spaces, maxOp):
+            k += 1
             if permutations[i][j] == "+":
-                tmpTot = tmpTot + int(operands[j+2])
+                tmpTot = tmpTot + int(operands[k])
             elif permutations[i][j] == "*":
                 tmpTot = tmpTot if tmpTot != 0 else 1
-                tmpTot = tmpTot * int(operands[j+2])
+                tmpTot = tmpTot * int(operands[k])
             else:
-                tmpTot = int(str(tmpTot)+operands[j+2])
-        if result == tmpTot:
-            tot += result
+                tmpTot = int(str(tmpTot)+operands[k])
+        
+        if int(operands[0][:-1]) == tmpTot:
+            tot += tmpTot
             break
 print(tot)
