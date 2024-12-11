578827c261b2cbd5e64e7b8dbadfb4dc3cfcee75
De Luca Riccardo <delucariccardo@hotmail.com>
2024-12-11 01:56:45 +0100

    day10 part 1 solved

---
+++ day10/part1.py
@@ -0,0 +1,47 @@
+def addNeighbor(next, paths, x, y):
+    if next not in paths:
+        paths.update({next: set()})
+    paths[next].add((x, y))    
+
+def findScore(trailHead):
+    paths = {}
+    moves = ((1, 0), (-1, 0), (0, 1), (0, -1))
+
+    def f(x, y, next):    
+        if next == 10:
+            return
+        
+        for m in moves:
+            r, c = x + m[0], y + m[1]
+            if 0 <= r < rows and 0 <= c < rows and trailMap[r][c] == str(next): 
+                addNeighbor(next, paths, r, c)
+        
+        if next not in paths:
+            return
+        
+        for e in paths[next]:
+            f(e[0], e[1], next+1)
+    
+    f(trailHead[0],  trailHead[1], 1)
+    return len(paths[9]) if 9 in paths else 0
+
+
+def part1():
+    scores = 0
+    while len(trailHeads) > 0:
+        scores += findScore(trailHeads.pop())
+    return scores
+        
+tmpFile = open("./day10/input.txt", "r")
+trailMap = tmpFile.readlines()
+tmpFile.close()
+rows = len(trailMap)
+cols = len(trailMap[0]) - 1
+
+trailHeads = set()
+for i in range(rows):
+    for j in range(cols):
+        if trailMap[i][j] == "0":
+            trailHeads.add((i, j))
+
+print(part1())
